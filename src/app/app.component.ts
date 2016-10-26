04f989e3e757937671344367ce32b87d0302a6f9
gersGitHub <gerblower@gmail.com>
2016-10-26 19:21:48 +0100

    changed message and gulped

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello Ger</h1>'
+    template: '<h1>Hello Galalal</h1>'
 })
 export class AppComponent { }
